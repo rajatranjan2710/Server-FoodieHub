ab09812f4e084ef22a8d2ca6b2f15b385eed6a22
Rajat Ranjan <rajatranjan477@gmail.com>
2023-09-08 00:33:14 +0530

    mongouri

--- config/database.js
+++ config/database.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async()=>{
 
-    const {connection} = await mongoose.connect(process.env.MONGO_URI);
+    const {connection} = await mongoose.connect("mongodb+srv://rajat27x:rajat2710@cluster0.0zws942.mongodb.net/foodiehub?retryWrites=true&w=majority");
     console.log(`Database is connected with ${connection.host}`)
 }
